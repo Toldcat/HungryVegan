91f212d2f0c34a6d8c86a110f5faf62d28e7bf1e
Toldcat <Andrew.avd@gmail.com>
2020-05-23 16:17:47 +0100

    optimised images

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Modal from 'react-modal'
 import { Link } from 'react-router-dom'
 
-const OptionModal = props => (
+const OptionModal = (props) => (
   <Modal isOpen={props.modalIsOpen} ariaHideApp={false} className='modal'>
     <h1 className='modal__heading'>Why Not Try {props.modalPick.name}?</h1>
     <p className='card__info modal__info'>
@@ -21,7 +21,11 @@ const OptionModal = props => (
       className='modal__link'
       href={`https://www.google.com/maps/search/?api=1&query=Hungry+Vegan&query_place_id=${props.modalPick.place_id}`}
     >
-      <img className='modal__icon' src='./images/mapsicon.png' />
+      <img
+        className='modal__icon'
+        alt='maps icon'
+        src='./images/mapsicon.png'
+      />
       Find it on Google Maps
     </a>
     <Link className='modal__link--small' to='/'>
